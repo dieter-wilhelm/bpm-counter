b3603307bcc8b46faea30155af44391809255853
dieter.wilhelm <dieter.wilhelm@aa01195e-e0d1-11dd-ab27-75d9513d8d6c>
2009-01-17 15:28:31 +0000

    first working trunk

--- bpm_counter.py
+++ bpm_counter.py
@@ -1,28 +1,46 @@
 #!/usr/bin/env python
-from time import *
+
+# TODO: 
+# restore terminal -> exception?
+# standard deviation checking
+
+#from time import *
+import time
 
 import curses
-stdscr = curses.initscr()
-curses.noecho()                 # do not show the keys
-curses.cbreak()                 # react to keys without Carriage return
-stdscr.keypad(1)                # return nice keyboard 
+stdscr = curses.initscr()      # this is the screen exactly the terminal size
+curses.noecho()                # do not show the keys
+curses.cbreak()                # react to keys without Carriage return
+stdscr.keypad(1)               # return nice keyboard shortcuts
 
-t0 = clock()
-while 1:
+ts = []
+#t0 = time.clock()               # time.clock() is the CPU time!
+
+t0 = time.time()          # time.time() is the Wall (real world) time!
+# (y,x) co-ordinates!
+stdscr.addstr(10,1,time.strftime("%Y-%m-%d",time.localtime())+
+              " Type 'q' to quit, 'n' to count.")
+
+c = stdscr.getch()
+t0 = time.time()          # time.time() is the Wall (real world) time!
+i = 0
+while 1 :
     c = stdscr.getch()
-    if c == ord('n'): t=clock();break
-    elif c == ord('q'): break  # Exit the while()
+    if c == ord( 'n'):
+        i = i + 1
+        t = time.time()
+        ts.append( t)
+        stdscr.addstr( 0, 1, "beats: " + str( i)) # beats
+#        s = time.strftime( "%S", time.gmtime( t - t0))
+        stdscr.addstr( 1, 1, "running " + str(t - t0) + " s")
+        stdscr.addstr( 2, 1, "beats per second " + str( 60*i / ( t - t0)) + " bpm")
+    elif c == ord( 'q') : break  # Exit the while()
 
-print "clock:", t - t0
-
-print "otto was here  !"
-
-sleep(10)
-
-# while 1:
-#     c = stdscr.getch()
-#     if c == ord('q'): break
-
+# use exceptions for restoring terminal stat!
 # the end, reversing the keyboard stuff
 curses.nocbreak(); stdscr.keypad(0); curses.echo()
 curses.endwin()                 # restore everything
+
+print "beats counted:", i
+print "beats per minute", str( 60*i / ( t - t0))
+print "standard deviation", str( 60*i / ( t - t0))
