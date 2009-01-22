d291cc67f008c289d9493f3e57cd46b9eaa38d11
dieter.wilhelm <dieter.wilhelm@aa01195e-e0d1-11dd-ab27-75d9513d8d6c>
2009-01-22 23:22:00 +0000

    introduced a stddev function

--- bpm_counter.py
+++ bpm_counter.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
 
 # TODO: 
-# standard deviation checking
-# documentation string
-# command line -h/--help option
-# option for counting time span and input of counts
+# standard deviation checking, dismiss pauses
+# display of overall and current bpm 
+# documentation string, command line -h/--help option
+
+def stddev( A):
+    """Guess what!"""
+    def mean( A):
+        """Guess what!"""
+        l = float( len( A))
+        return sum( A) / l
+    m = mean( A)
+    l = float( len( A))
+    v = 0
+    for x in A:
+       v = v + ( x - m)**2
+    return v / l
 
 #from time import *
 import time
+# t = time.clock()         # time.clock() is the CPU time!
+# t = time.time()          # time.time() is the Wall (real world) time!
 
 import curses
 stdscr = curses.initscr()      # this is the screen exactly the terminal size
@@ -16,30 +30,38 @@ curses.cbreak()                # react to keys without Carriage return
 stdscr.keypad(1)               # return nice keyboard shortcuts
 
 try:
+    r = 40, 200                 # interesting beat range
     ts = []
-#t0 = time.clock()               # time.clock() is the CPU time!
-    t0 = time.time()          # time.time() is the Wall (real world) time!
-# (y,x) co-ordinates!
+
+    # addstr uses (y,x) co-ordinates!
     stdscr.addstr(10,1,time.strftime("%Y-%m-%d",time.localtime())+ \
                       " Type 'q' to quit, 'n' to count.")
-    c = stdscr.getch()
-    t0 = time.time()          # time.time() is the Wall (real world) time!
+
     i = 0
+    c = stdscr.getch()
+    # beginning
+    t = t0 = time.time()          # time.time() is the Wall (real world) time!
+    t1 = t2 = time.time()
+    if c == ord('q'):
+        raise
     while 1 :
         c = stdscr.getch()
         if c == ord( 'n'):
             i = i + 1
             t = time.time()
-            ts.append( t)
+            t2 = t
+            ts.append( t2 - t1)
             stdscr.addstr( 0, 1, "beats: " + str( i)) # beats
         #        s = time.strftime( "%S", time.gmtime( t - t0))
             stdscr.addstr( 1, 1, "running " + str(t - t0) + " s")
             stdscr.addstr( 2, 1, "beats per second " + str( 60*i / ( t - t0)) + " bpm")
+            t1 = t2
         elif c == ord( 'q') : break  # Exit the while()
 finally:
     # the end, reversing the keyboard stuff
     curses.nocbreak(); stdscr.keypad(0); curses.echo()
     curses.endwin()                 # restore everything
     print "beats counted:", i
-    print "beats per minute", str( 60*i / ( t - t0))
-    print "standard deviation", str( 60*i / ( t - t0))
+    if t != t0:
+        print "Overall:", str( 60*i / ( t - t0)), "bpm"
+        print "Standard deviation", str( stddev( ts) * 60), "bpm"
