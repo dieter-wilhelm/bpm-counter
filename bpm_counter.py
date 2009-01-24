af78d390a3bdf8ed4cf1d9e5393a051589e5484b
dieter.wilhelm <dieter.wilhelm@aa01195e-e0d1-11dd-ab27-75d9513d8d6c>
2009-01-24 18:10:18 +0000

    corrected stddev()

--- bpm_counter.py
+++ bpm_counter.py
@@ -1,27 +1,49 @@
 #!/usr/bin/env python
 
-# TODO: 
-# standard deviation checking, dismiss pauses
-# display of overall and current bpm 
+# TODO:
+# moving average as current average?
+# restart key with status of last measurement
+# dismiss pauses/unregular typing  
 # documentation string, command line -h/--help option
 
-def stddev( A):
-    """Guess what!"""
-    def mean( A):
-        """Guess what!"""
-        l = float( len( A))
-        return sum( A) / l
+import math
+
+def mean( A):
+    """Mean of the list A."""
+    l = float( len( A))
+    return sum( A) / l
+
+def var( A):
+    """Variance (n-1) of the list A.
+Returns 0 if len( A) < 1.
+    """
     m = mean( A)
     l = float( len( A))
     v = 0
     for x in A:
        v = v + ( x - m)**2
-    return v / l
+    if l > 1:
+        return v / ( l - 1)
+    else:
+        return 0.
 
+def stddev( A):
+    """Standard deviation of the list A."""
+    return math.sqrt( var( A))
+    
 #from time import *
 import time
-# t = time.clock()         # time.clock() is the CPU time!
-# t = time.time()          # time.time() is the Wall (real world) time!
+ # time.clock() is the CPU time!
+ # time.time() is the Wall (real world) time!
+import string
+
+
+class StopWatch:
+    """Ahhh, well."""
+    ts = []                     # the times
+
+
+# --- interface stuff ---
 
 import curses
 stdscr = curses.initscr()      # this is the screen exactly the terminal size
@@ -39,6 +61,7 @@ try:
 
     i = 0
     c = stdscr.getch()
+    stdscr.addstr(0, 1, "beat : 1")
     # beginning
     t = t0 = time.time()          # time.time() is the Wall (real world) time!
     t1 = t2 = time.time()
@@ -49,12 +72,18 @@ try:
         if c == ord( 'n'):
             i = i + 1
             t = time.time()
-            t2 = t
+            t2 = t              # t2 is current time t1 the time before
             ts.append( t2 - t1)
-            stdscr.addstr( 0, 1, "beats: " + str( i)) # beats
+            stdscr.addstr( 0, 1, "No of beats: " + str( i)) # beats
         #        s = time.strftime( "%S", time.gmtime( t - t0))
-            stdscr.addstr( 1, 1, "running " + str(t - t0) + " s")
-            stdscr.addstr( 2, 1, "beats per second " + str( 60*i / ( t - t0)) + " bpm")
+            stdscr.addstr( 1, 1, "Running for " + str( round( t2 - t0, 1)) + " s")
+            b =  60*i / ( t - t0)
+            stdscr.addstr( 2, 1, "Overall beats per minute: " + string.rjust( str( round( b, 1)), 5) + " bpm")
+            stdscr.addstr( 3, 1, "Current beats per minute: " + string.rjust( str( round( 60 / ( t2 - t1), 1)), 5) + " bpm")
+            if i > 1:
+                d = stddev( ts) * 60
+                stdscr.addstr( 4, 1, "Overall stddev: " + string.rjust( str( round( d, 1)), 4) + " bpm")
+                stdscr.addstr( 5, 1, "Relative dev: " + string.rjust( str( round(d / b * 100, 1)), 4) + " %")
             t1 = t2
         elif c == ord( 'q') : break  # Exit the while()
 finally:
@@ -63,5 +92,5 @@ finally:
     curses.endwin()                 # restore everything
     print "beats counted:", i
     if t != t0:
-        print "Overall:", str( 60*i / ( t - t0)), "bpm"
-        print "Standard deviation", str( stddev( ts) * 60), "bpm"
+        print "Overall:", str( round( 60*i / ( t - t0), 1)), "bpm"
+        print "Standard deviation", str( round( stddev( ts) * 60, 2)), "bpm"
